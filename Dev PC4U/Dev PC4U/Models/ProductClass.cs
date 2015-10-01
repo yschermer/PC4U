05d8c07df1676242c6f797b2eb6605eba651d0cd
jadejager <jager.l@hotmail.com>
2015-10-01 12:28:43 +0200

    ProductClass.cs toegevoegd

---
+++ Dev PC4U/Dev PC4U/Models/ProductClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dev_PC4U.Models
+{
+    public class ProductClass
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
